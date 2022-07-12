922beb31c90a1e4cec2a763f3557565e07676d56
KavinTheG <kavin.balag@gmail.com>
2022-07-12 12:32:54 -0400

    Test

--- remove_archive.sh
+++ remove_archive.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-$expiry_date = date --date="5 week ago" +%F
+expiry_date=$(date --date="5 week ago" +%F)
 
+echo "removing backup_log-${expiry_date}.tar.gz"
 # Remove tar archives from 5 weeks ago
-find /backup_logs/  -name "backup_log-$expiry_date.tar.gz" -exec rm {} \;
+find /backup_logs/  -name "backup_log-${expiry_date}.tar.gz" -exec rm {} \;
