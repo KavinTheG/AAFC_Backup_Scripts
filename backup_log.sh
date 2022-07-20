7bee2e59ff9b1438a8791ee9983f02351da414d3
KavinTheG <kavin@balag.gmail.com>
2022-07-20 14:40:34 -0400

    Changed naming scheme

--- backup_log.sh
+++ backup_log.sh
@@ -1,13 +1,24 @@
 #!/bin/bash 
 
+# Path to desired backup directory
 src_dir=/
+
+# Extension
+ext=.log
+
+# Title of directory
+dir_title=backup_log_$(date +%F)
 
 # Checks if an argument was given
 if [ $# -eq 0 ]; then 
-	echo "Source directory was not given. Assume /"
+	echo "Source directory was not given. Backing up $ext files from root ($src_dir)"
+	dir_title=backup_log_,_$(date +%F)
 else 
-	src_dir= $1 
+	src_dir=$1 
 	echo "Backuping log files at directory $src_dir"
+
+	# Replace '/' with '|'
+	dir_title=backup_log_"${src_dir////,}"_"$(date +%F)"
 fi
 
 
@@ -17,23 +28,22 @@ if [ ! -d /backup_logs ]; then
 fi
 
 # Make a new directory
-mkdir /backup_logs/backup_log_"$(date +%F)"
+echo "Making directory $dir_title"
+sleep 3
+mkdir /backup_logs/$dir_title
 
 # -a -> archive
 # -v -> verbose
-find $src_dir -type f -name "*.log" -exec rsync -av --exclude={/backups_logs} '{}' /backup_logs/backup_log_"$(date +%F)" \;
+find $src_dir -type f -name "*.log" -exec rsync -av --exclude={/backups_logs} '{}' /backup_logs/"$dir_title" \;
 
 # -z -> gzip
 # -c -> create archive
 # -v -> verbose
 # -f -> the name
 
-if [[ -d /backup_logs/backup_log_"$(date +%F)" ]] 
+if [[ -d /backup_logs/$dir_title ]] 
 then
-	tar -zcvf /backup_logs/backup_log_$(date +%F).tar.gz /backup_logs/backup_log_$(date +%F) --remove-files
-
-	# Remove new directory
-	# rm -r /backup_logs/backup_log_"$(date +%F)"
+	tar -zcvf /backup_logs/$dir_title.tar.gz /backup_logs/$dir_title --remove-files
 else
-	echo "Backup directory backup_log-$(date +%F) was not created"
+	echo "Backup directory $dir_title was not created"
 fi
