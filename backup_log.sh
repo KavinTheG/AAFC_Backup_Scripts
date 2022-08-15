9c9f43fde3a96c7199a466251e4149a50a44f332
KavinTheG <kavin@balag.gmail.com>
2022-08-15 12:21:57 -0400

    Changed naming convention, added remove_archive.sh functionality to backup_log.sh. Now backup_log.sh will remove old archives as well

--- backup_log.sh
+++ backup_log.sh
@@ -9,16 +9,19 @@ ext=.log
 # Title of directory
 dir_title=backup_log_$(date +%F)
 
+# Get the date of older backups to remove
+expiry_date=$(date --date="5 week ago" +%F)
+
 # Checks if an argument was given
 if [ $# -eq 0 ]; then 
 	echo "Source directory was not given. Backing up $ext files from root ($src_dir)"
-	dir_title=backup_log_,_$(date +%F)
+	dir_title=backup_log_$(date +%F)
 else 
 	src_dir=$1 
 	echo "Backuping log files at directory $src_dir"
 
 	# Replace '/' with '|'
-	dir_title=backup_log_"${src_dir////,}"_"$(date +%F)"
+	dir_title=backup_log"${src_dir////_}"_"$(date +%F)"
 fi
 
 
@@ -47,3 +50,7 @@ then
 else
 	echo "Backup directory $dir_title was not created"
 fi
+
+
+echo "removing backup_log-${expiry_date}.tar.gz"
+find /backup_logs/  -name "*${expiry_date}.tar.gz" -exec rm {} \;
